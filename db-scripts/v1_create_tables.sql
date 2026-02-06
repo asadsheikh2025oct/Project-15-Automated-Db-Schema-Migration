9cc0f351ef8fd5fe9617e1cda9dfb8a92ac59b5f
asadsheikh2025oct <asadsheikh2025oct@outlook.com>
2026-02-06 15:23:18 +0000

    idempotency based sql logic

--- db-scripts/v1_create_tables.sql
+++ db-scripts/v1_create_tables.sql
@@ -1,1 +1,9 @@
-CREATE TABLE Inventory (Id INT PRIMARY KEY, ItemName NVARCHAR(50), Quantity INT);
+IF NOT EXISTS (SELECT * FROM sys.tables WHERE name = 'Inventory')
+BEGIN
+    CREATE TABLE Inventory (
+        Id INT PRIMARY KEY, 
+        ItemName NVARCHAR(50), 
+        Quantity INT,
+        LastUpdated DATETIME DEFAULT GETDATE()
+    );
+END
